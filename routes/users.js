dd9a7b55f68503f4aae61665d17590c1d10eb80b
Irakliy Dochviri <irakliy.d@yandex.ru>
2021-09-09 00:29:37 +0300

    fix: убрать userId валидацию #2

--- routes/users.js
+++ routes/users.js
@@ -1,11 +1,11 @@
 const router = require('express').Router();
-const { validateUserUpdate, validateUserId } = require('../middlewares/validation');
+const { validateUserUpdate } = require('../middlewares/validation');
 
 const {
   getUser, updateUser,
 } = require('../controllers/users');
 
-router.get('/me', validateUserId, getUser);
+router.get('/me', getUser);
 router.patch('/me', validateUserUpdate, updateUser);
 
 module.exports = router;
