68ead810021aabb3d08824179f09390ad6a87133
Irakliy Dochviri <irakliy.d@yandex.ru>
2021-10-19 23:56:23 +0300

    fix: изменить CORS #2

--- routes/users.js
+++ routes/users.js
@@ -2,10 +2,10 @@ const router = require('express').Router();
 const { validateUserUpdate } = require('../middlewares/validation');
 
 const {
-  getUser, updateUser, _getUsers,
+  getUser, updateUser,
 } = require('../controllers/users');
 
-router.get('/', _getUsers);
+// router.get('/', _getUsers);
 
 router.get('/me', getUser);
 router.patch('/me', validateUserUpdate, updateUser);
