ec8b3aec74852a67e1261585a69a4c8b01e79b29
Irakliy Dochviri <irakliy.d@yandex.ru>
2021-10-20 02:21:02 +0300

    fix: исправить ошибку CORS #4

--- routes/users.js
+++ routes/users.js
@@ -5,8 +5,6 @@ const {
   getUser, updateUser,
 } = require('../controllers/users');
 
-// router.get('/', _getUsers);
-
 router.get('/me', getUser);
 router.patch('/me', validateUserUpdate, updateUser);
 
